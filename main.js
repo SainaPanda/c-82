164f6e3794cb53de97376342753006623218507e
SainaPanda <88354134+SainaPanda@users.noreply.github.com>
2022-02-26 13:56:52 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,60 +1,62 @@
-var mouseEvent= "empty";
+var mouseEvent = "empty";
 var last_position_of_x, last_position_of_y;
 
     canvas = document.getElementById('myCanvas');
     ctx = canvas.getContext("2d");
-
+    
     color = "black";
     width_of_line = 1;
     canvas.addEventListener("mousedown", my_mousedown);
     function my_mousedown(e)
     {
+        //Addictonal Activity start
         color = document.getElementById("color").value;
         width_of_line = document.getElementById("width_of_line").value;
-        
+        //Addictonal Activity ends
+
         mouseEvent = "mouseDown";
-        console.log(mouseEvent);
     }
+
     canvas.addEventListener("mousemove", my_mousemove);
     function my_mousemove(e)
     {
-        current_pos_x = e.clientX - canvas.offsetLeft;
-        current_pos_y = e.clientY - canvas.offsetTop;
+        current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
+        current_position_of_mouse_y = e.clientY - canvas.offsetTop;
 
-        if(mouseEvent == "mouseDown") {
-            ctx.beginPath();
-            ctx.strokeStyle = color;
-            ctx.lineWidth = width_of_line;
+        if (mouseEvent == "mouseDown") {
+        ctx.beginPath();
+        ctx.strokeStyle = color;
+        ctx.lineWidth = width_of_line;
 
-            console.log("Last position of x and y coordinates = ");
-            console.log("x =" + last_position_of_x + "y = " + last_position_of_y);
-           ctx.moveTo(last_position_of_x, last_position_of_y);
+        console.log("Last position of x and y coordinates = ");
+        console.log("x = " + last_position_of_x + "y = " + last_position_of_y);
+        ctx.moveTo(last_position_of_x, last_position_of_y);
 
-           console.log("current position of x and y coordinates = ");
-            console.log("x =" + current_pos_x + "y = " + current_pos_y);
-           ctx.lineTo(current_pos_x, current_pos_y);
-            ctx.stroke;
+        console.log("Current position of x and y coordinates = ");
+        console.log("x  = " + current_position_of_mouse_x + "y = " + current_position_of_mouse_y);
+        ctx.lineTo(current_position_of_mouse_x, current_position_of_mouse_y);
+        ctx.stroke();
         }
 
-        last_position_of_x = current_pos_x;
-        last_position_of_y = current_pos_y;
+        last_position_of_x = current_position_of_mouse_x; 
+        last_position_of_y = current_position_of_mouse_y;
     }
+
     canvas.addEventListener("mouseup", my_mouseup);
     function my_mouseup(e)
     {
         mouseEvent = "mouseUP";
-        console.log(mouseEvent);
     }
-    
+
     canvas.addEventListener("mouseleave", my_mouseleave);
     function my_mouseleave(e)
     {
         mouseEvent = "mouseleave";
-        console.log(mouseEvent);
     }
 
-    function clearArea()
-    {
-        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-    }
+//Additional Activity
+function clearArea() {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
+
